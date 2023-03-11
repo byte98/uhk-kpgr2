1b9f8c070bb1a59f6dd7504d2689685f7324447c
Jiří Škoda <developer@skodaj.cz>
2023-03-11 11:53:05 +0100

    Triangle rasterization implemented
    Yes, finally it works. I cannot believe it, but triangle rasterization now works.

--- ZBUFFER/ZBuffer/src/main/java/cz/uhk/fim/skodaji1/kpgr2/zbuffer/raster/TriangleRasterizer.java
+++ ZBUFFER/ZBuffer/src/main/java/cz/uhk/fim/skodaji1/kpgr2/zbuffer/raster/TriangleRasterizer.java
@@ -17,7 +17,12 @@
  */
 package cz.uhk.fim.skodaji1.kpgr2.zbuffer.raster;
 
+import cz.uhk.fim.kpgr2.transforms.Col;
 import cz.uhk.fim.kpgr2.transforms.Point3D;
+import java.awt.Color;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.function.Function;
 
 /**
@@ -63,64 +68,37 @@ public class TriangleRasterizer extends AbstractRasterizer
         int minY = Integer.min(y1, Integer.min(y2, y3));
         int dX = maxX - minX;
         int dY = maxY - minY;
+        Point3D[] points = new Point3D[]{new Point3D(x1, y1, z1), new Point3D(x2, y2, z2), new Point3D(x3, y3, z3)};
         if (dX > dY)
         {
             // Sort points according to X axis
-            Point3D p1 = new Point3D(x1, y1, z1);
-            Point3D p2 = new Point3D(x2, y2, z2);
-            Point3D p3 = new Point3D(x3, y3, z3);
-            if (p3.x < p2.x)
-            {
-                Point3D tmp = p2;
-                p2 = p3;
-                p3 = tmp;
-            }
-            if (p2.x < p1.x)
-            {
-                Point3D tmp = p1;
-                p1 = p2;
-                p2 = tmp;
-            }
-            if (p3.x < p2.x)
-            {
-                Point3D tmp = p2;
-                p2 = p3;
-                p3 = tmp;
-            }
+            Arrays.sort(points, 0, points.length, new Comparator<Point3D>(){
+                @Override
+                public int compare(Point3D t, Point3D t1)
+                {
+                    return (int)Math.round(t1.x - t.x);
+                }
+            });
             this.rasterizeX(
-                    p1.x, p1.y, p1.z,
-                    p2.x, p2.y, p2.z,
-                    p3.x, p3.y, p3.z
+                    points[0].x, points[0].y, points[0].z,
+                    points[1].x, points[1].y, points[1].z,
+                    points[2].x, points[2].y, points[2].z
             );
         }
         else
         {
             // Sort points according to Y axis
-            Point3D p1 = new Point3D(x1, y1, z1);
-            Point3D p2 = new Point3D(x2, y2, z2);
-            Point3D p3 = new Point3D(x3, y3, z3);
-            if (p3.y < p2.y)
-            {
-                Point3D tmp = p2;
-                p2 = p3;
-                p3 = tmp;
-            }
-            if (p2.y < p1.y)
-            {
-                Point3D tmp = p1;
-                p1 = p2;
-                p2 = tmp;
-            }
-            if (p3.y < p2.y)
-            {
-                Point3D tmp = p2;
-                p2 = p3;
-                p3 = tmp;
-            }
+            Arrays.sort(points, 0, points.length, new Comparator<Point3D>(){
+                @Override
+                public int compare(Point3D t, Point3D t1)
+                {
+                    return (int)Math.round(t.y - t1.y);
+                }
+            });
             this.rasterizeY(
-                    p1.x, p1.y, p1.z,
-                    p2.x, p2.y, p2.z,
-                    p3.x, p3.y, p3.z
+                    points[0].x, points[0].y, points[0].z,
+                    points[1].x, points[1].y, points[1].z,
+                    points[2].x, points[2].y, points[2].z
             );
         }
     }
@@ -139,29 +117,79 @@ public class TriangleRasterizer extends AbstractRasterizer
      */
     private void rasterizeX(double x1, double y1, double z1, double x2, double y2, double z2, double x3, double y3, double z3)
     {
-        double x = x1;
-        double stepY2 = (y2 - y1)/(x2 - x1);
-        double stepY3 = (y3 - y1)/(x3 - x1);
-        double stepZ2 = (z2 - z1)/(x2 - x1);
-        double stepZ3 = (z3 - z1)/(x3 - x1);
-        double stepY23 = (y3 - y2)/(x3 - x2);
-        double stepZ23 = (z3 - z2)/(x3 - x2);
-        // X1 -> X2
-        for(; x < x2; x++)
+        // <editor-fold defaultstate="collapsed" desc="X1 -> X2">
+        
+        // Deltas from X1 to X2
+        double dX1X2     = x2 - x1;
+        double dY1Y2     = y2 - y1;
+        double dZ1Z2     = z2 - z1;
+        int    rangeX1X2 = (int) Math.round(Math.abs(dX1X2));
+        
+        // Deltas from X1 to X3
+        double dX1X3     = x3 - x1;
+        double dY1Y3     = y3 - y1;
+        double dZ1Z3     = z3 - z1;
+        int    rangeX1X3 = (int) Math.round(Math.abs(dX1X3));
+        
+        // Steps from X1 to X2
+        double stepX1X2  = dX1X2 < 0 ? -1 : 1;
+        double stepY1Y2  = dY1Y2 / rangeX1X2;
+        double stepZ1Z2  = dZ1Z2 / rangeX1X2;
+        
+        // Steps from X1 to X3
+        double stepX1X3  = dX1X3 < 0 ? -1 : 1;
+        double stepY1Y3  = dY1Y3 / rangeX1X3;
+        double stepZ1Z3  = dZ1Z3 / rangeX1X3;
+        
+        // Draw loop
+        for (int i = 0; i < rangeX1X2; i++)
         {
+            int    Ax = (int) Math.round(x1 + (i * stepX1X2));
+            int    Ay = (int) Math.round(y1 + (i * stepY1Y2));
+            double Az = z1 + (i * stepZ1Z2);
+            
+            int    Bx = (int) Math.round(x1 + (i * stepX1X3));
+            int    By = (int) Math.round(y1 + (i * stepY1Y3));
+            double Bz = z1 + (i * stepZ1Z3);
+            
             this.lineRasterizer.rasterize(
-                    (int)Math.round(x), (int)Math.round(y1 + (x * stepY3)), z1 + (x * stepZ3),
-                    (int)Math.round(x), (int)Math.round(y1 + (x * stepY2)), z1 + (x * stepZ2)
+                    Ax, Ay, Az,
+                    Bx, By, Bz
             );
         }
-        // X2 -> X3
-        for(; x < x3; x++)
+        
+        // </editor-fold>
+        // <editor-fold defaultstate="collapsed" desc="X2 -> X3">
+        
+        // Deltas from Y2 to Y3
+        double dX2X3     = x3 - x2;
+        double dY2Y3     = y3 - y2;
+        double dZ2Z3     = z3 - z2;
+        int    rangeX2X3 = (int)Math.round(Math.abs(dX2X3));
+        
+        // Steps from Y2 to Y2
+        double stepX2X3  = dX2X3 < 0 ? -1 : 1;
+        double stepY2Y3  = dY2Y3 / rangeX2X3;
+        double stepZ2Z3  = dZ2Z3 / rangeX2X3;
+        
+        // Draw loop
+        for (int i = 0; i < rangeX2X3; i++)
         {
+            int    Ax = (int) Math.round(x2 + (i * stepX2X3));
+            int    Ay = (int) Math.round(y2 + (i * stepY2Y3));
+            double Az = z2 + (i * stepZ2Z3);
+            
+            int    Bx = (int) Math.round(x1 + ((rangeX1X2 + i) * stepX1X3));
+            int    By = (int) Math.round(y1 + ((rangeX1X2 + i) * stepY1Y3));
+            double Bz = z1 + ((rangeX1X2 + i) * stepZ1Z3);
+            
             this.lineRasterizer.rasterize(
-                    (int)Math.round(x), (int)Math.round(y1 + (x * stepY3)), z1 + (x * stepZ3),
-                    (int)Math.round(x), (int)Math.round(y2 + (x * stepY23)), z2 + (x * stepZ23)
+                    Ax, Ay, Az,
+                    Bx, By, Bz
             );
         }
+        
+        // </editor-fold>
     }
     
     /**
@@ -178,23 +206,78 @@ public class TriangleRasterizer extends AbstractRasterizer
      */
     private void rasterizeY(double x1, double y1, double z1, double x2, double y2, double z2, double x3, double y3, double z3)
     {
-        double y = y1;
-        double stepX2 = (x2 - x1)/(y2 - y1);
-        double stepX3 = (x3 - x1)/(y3 - y1);
-        double stepZ2 = (z2 - z1)/(y2 - y1);
-        double stepZ3 = (z3 - z1)/(y3 - y1);
-        // Y1 -> Y2
-        for(y = y1; y < y2; y++)
+        // <editor-fold defaultstate="collapsed" desc="Y1 -> Y2">
+        
+        // Deltas from Y1 to Y2
+        double dX1X2     = x2 - x1;
+        double dY1Y2     = y2 - y1;
+        double dZ1Z2     = z2 - z1;
+        int    rangeY1Y2 = (int) Math.round(Math.abs(dY1Y2));
+        
+        // Deltas from Y1 to Y3
+        double dX1X3     = x3 - x1;
+        double dY1Y3     = y3 - y1;
+        double dZ1Z3     = z3 - z1;
+        int    rangeY1Y3 = (int) Math.round(Math.abs(dY1Y3));
+        
+        // Steps from Y1 to Y2
+        double stepX1X2  = dX1X2 / rangeY1Y2;
+        double stepY1Y2  = dY1Y2 < 0 ? -1 : 1;
+        double stepZ1Z2  = dZ1Z2 / rangeY1Y2;
+        
+        // Steps from Y1 to Y3
+        double stepX1X3  = dX1X3 / rangeY1Y3;
+        double stepY1Y3  = dY1Y3 < 0 ? -1 : 1;
+        double stepZ1Z3  = dZ1Z3 / rangeY1Y3;
+        
+        // Draw loop
+        for (int i = 0; i < rangeY1Y2; i++)
         {
-            int xA = (int) Math.round(x1 + (y * stepX2));
-            int yA = (int) Math.round(y);
-            double zA = z1 + (y * stepZ2);
+            int    Ax = (int) Math.round(x1 + (i * stepX1X2));
+            int    Ay = (int) Math.round(y1 + (i * stepY1Y2));
+            double Az = z1 + (i * stepZ1Z2);
             
-            int xB = (int) Math.round(x1 + (y * stepX3));
-            int yB = (int) Math.round(y);
-            double zB = z1 + (y * stepZ3);
+            int    Bx = (int) Math.round(x1 + (i * stepX1X3));
+            int    By = (int) Math.round(y1 + (i * stepY1Y3));
+            double Bz = z1 + (i * stepZ1Z3);
             
-            this.lineRasterizer.rasterize(xA, yA, zA, xB, yB, zB);
+            this.lineRasterizer.rasterize(
+                    Ax, Ay, Az,
+                    Bx, By, Bz
+            );
         }
+        
+        // </editor-fold>
+        // <editor-fold defaultstate="collapsed" desc="Y2 -> Y3">
+        
+        // Deltas from Y2 to Y3
+        double dX2X3     = x3 - x2;
+        double dY2Y3     = y3 - y2;
+        double dZ2Z3     = z3 - z2;
+        int    rangeY2Y3 = (int)Math.round(Math.abs(dY2Y3));
+        
+        // Steps from Y2 to Y2
+        double stepX2X3  = dX2X3 / rangeY2Y3;
+        double stepY2Y3  = dY2Y3 < 0 ? -1 : 1;
+        double stepZ2Z3  = dZ2Z3 / rangeY2Y3;
+        
+        // Draw loop
+        for (int i = 0; i < rangeY2Y3; i++)
+        {
+            int    Ax = (int) Math.round(x2 + (i * stepX2X3));
+            int    Ay = (int) Math.round(y2 + (i * stepY2Y3));
+            double Az = z2 + (i * stepZ2Z3);
+            
+            int    Bx = (int) Math.round(x1 + ((rangeY1Y2 + i) * stepX1X3));
+            int    By = (int) Math.round(y1 + ((rangeY1Y2 + i) * stepY1Y3));
+            double Bz = z1 + ((rangeY1Y2 + i) * stepZ1Z3);
+            
+            this.lineRasterizer.rasterize(
+                    Ax, Ay, Az,
+                    Bx, By, Bz
+            );
+        }
+        
+        // </editor-fold>
     }
 }
